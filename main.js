9169a183d5e530558e58b3c1fa35aec3fde5b69c
MarinKhar26 <viktorovna160196@gmail.com>
2023-02-03 15:48:06 +0200

    created userYear and age

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ let day = +prompt('What day were you born?')
 
 let month = +prompt('What month were you born?')
 
+let userYear = +prompt('What year were you born?')
+const year = 2022
+let age = year - userYear
+
