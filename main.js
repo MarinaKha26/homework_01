e8e7ba2dbee281d3fef0d169464dc389248f425b
MarinaKha26 <viktorovna160196@gmail.com>
2023-02-14 00:34:11 +0200

    created array

--- main.js
+++ main.js
@@ -5,4 +5,9 @@ while (( userValue <2) || ( userValue > 10)) {
     alert('Please enter another value')
    userValue = +prompt('Enter the length of the array')
    userValue = Math.abs(Math.round(userValue))
-} 
+}
+
+let newArr = []
+ for ( let i = 0; i < userValue; i++){
+        newArr[i] = Math.round(Math.random()*10)
+} console.log(newArr)
