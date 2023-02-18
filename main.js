77e8b1cb7f46294380f35dadba10c0683b9a8e00
MarinKhar26 <viktorovna160196@gmail.com>
2023-02-19 01:05:07 +0200

    added arrays

---
+++ main.js
@@ -0,0 +1,20 @@
+const animals = [
+	['🐭','mouse','Jerry'],
+	['🐹','hamster','Biscuit'],
+	['🐰','rabbit','Bugs'],
+	['🦊','fox','Mrs. Fox'],
+	['🐻','bear','Paddington']
+];
+
+const food = [
+	['🍎','apple',10],
+	['🍐','pear',12],
+	['🍊','tangerine',15],
+	['🍋','lemon',5],
+	['🍌','banana',7]
+];
+
+const universes = [
+	['🖤', 'DC', ['Superman', 'Batman', 'Wonder Woman']],
+	['❤️', 'Marvel', ['Iron Man', 'the Hulk', 'Black Widow']]
+]
