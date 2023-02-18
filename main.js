c7b331b7bd5ee55ece65880ce7a52304e7c3c06c
MarinKhar26 <viktorovna160196@gmail.com>
2023-02-19 01:06:01 +0200

    created function

--- main.js
+++ main.js
@@ -18,3 +18,32 @@ const universes = [
 	['🖤', 'DC', ['Superman', 'Batman', 'Wonder Woman']],
 	['❤️', 'Marvel', ['Iron Man', 'the Hulk', 'Black Widow']]
 ]
+
+function getInfo(arr, arrHeader){
+	let tbl = document.createElement("table");
+	let tblBody = document.createElement("tbody");
+	
+	let tblHead = document.createElement("thead");
+	let headText = document.createTextNode(arrHeader)
+	tblHead.appendChild(headText);
+	tbl.appendChild(tblHead);
+
+	for (i = 0 ; i < arr.length; i++){
+		let row = document.createElement("tr");
+		for (b = 0; b < arr[i].length; b++){
+			let cell = document.createElement("td");
+			if(Array.isArray(arr[i][b])){
+				newCell = arr[i][b].join('; ')
+			}else{newCell = arr[i][b]}
+        	let cellText = document.createTextNode(newCell)
+        	cell.appendChild(cellText);
+        	row.appendChild(cell);
+	    }
+
+		tblBody.appendChild(row);
+	}
+
+	tbl.appendChild(tblBody);
+
+	return tbl
+}
