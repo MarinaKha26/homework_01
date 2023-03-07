8727b5e46b34d43fd49708a18802c7015542a892
MarinaKha26 <viktorovna160196@gmail.com>
2023-03-07 17:25:16 +0200

    create class for stuffing

---
+++ main.js
@@ -0,0 +1,5 @@
+class Stuffing {
+    static STUFFING_CHEESE = {name: 'STUFFING_CHEESE', price: 10, calories: 20}
+    static STUFFING_SALAD = {name: 'STUFFING_SALAD', price: 20, calories: 5}
+    static STUFFING_POTATO = {name: 'STUFFING_POTATO', price: 15, calories: 10}
+}
